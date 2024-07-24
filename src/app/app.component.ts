acfadfa97cf49bfa6b6aa5bc7dc4d3dbee04c6b5
Nino Zullo <nino.zullo05dev@gmail.com>
2024-07-24 17:19:12 +0200

    trying to fix routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { SignUpComponent } from './models/auth/sign-up/sign-up.component';
+import { SignInComponent } from './models/auth/sign-in/sign-in/sign-in.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SignUpComponent, RouterLink, RouterLinkActive],
+  imports: [
+    RouterOutlet,
+    SignUpComponent,
+    RouterLink,
+    RouterLinkActive,
+    SignInComponent,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignUpComponent } from './models/auth/sign-up/sign-up.component';
 import { SignInComponent } from './models/auth/sign-in/sign-in/sign-in.component';
 
 export const routes: Routes = [
+  { path: '', component: SignUpComponent },
   { path: 'signUp', component: SignUpComponent },
   { path: 'signIn', component: SignInComponent },
 ];
